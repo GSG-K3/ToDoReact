df8ff6cf674e9b99640c6e78e672826fb2575a58
karmelyoei <karmel.salah@gmail.com>
2020-03-25 14:54:33 +0200

    added the component elements with a fake data and a simple deisgn #2 #3

---
+++ my-first-react/src/components/todoData.js
@@ -0,0 +1,19 @@
+const todoData = [
+  {
+    id: 1,
+    task: 'Wake up at 7:00 Am',
+    completed: false
+  },
+  {
+    id: 2,
+    task: 'Cleaning the House',
+    completed: true
+  },
+  {
+    id: 3,
+    task: 'Do my home Work',
+    completed: true
+  }
+];
+
+export default todoData;
